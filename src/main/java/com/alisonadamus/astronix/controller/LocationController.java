222ecfc11125be606d9288def8fb0b8211dd64a9
alisonadamus <alisaadamus.aa@gmail.com>
2026-05-12 20:55:53 +0300

    added a location details page with a list of materials

--- src/main/java/com/alisonadamus/astronix/controller/LocationController.java
+++ src/main/java/com/alisonadamus/astronix/controller/LocationController.java
@@ -1,10 +1,14 @@
 package com.alisonadamus.astronix.controller;
 
+import com.alisonadamus.astronix.model.Location;
 import com.alisonadamus.astronix.service.LocationService;
+import com.alisonadamus.astronix.service.MaterialService;
+import com.alisonadamus.astronix.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class LocationController {
 
     private final LocationService locationService;
+    private final MaterialService materialService;
+    private final TaskService taskService;
 
     @GetMapping
     public String showLocationsPage(@RequestParam(value = "search", required = false) String search, Model model) {
@@ -21,5 +27,16 @@ public class LocationController {
         model.addAttribute("searchQuery", search);
         model.addAttribute("showSearch", true);
         return "locations";
+    }
+
+    @GetMapping("/{id}")
+    public String showLocationDetails(@PathVariable Long id, Model model) {
+        Location location = locationService.getById(id);
+
+        model.addAttribute("location", location);
+        model.addAttribute("materials", materialService.getByLocation(id));
+        model.addAttribute("tasks", taskService.getTasksByLocation(id));
+
+        return "location-details";
     }
 }
